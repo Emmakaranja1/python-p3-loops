dba1e06456f33cd697a8cabd2b13deddb0fcf20c
Emmakaranja1 <karanjaemmak@gmail.com>
2025-05-08 16:17:10 +0300

    Adds countdown  using while loop

--- lib/looping.py
+++ lib/looping.py
@@ -2,11 +2,18 @@
 
 def happy_new_year():
     # code goes here!
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
     pass
+
 
 def square_integers(int_list):
     # code goes here!
     pass
+
 
 def fizzbuzz():
     # code goes here!
